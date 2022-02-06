39a110e74ffe7370ea33177e27d638ae7d78f417
Johnwick9581 <99114187+Johnwick9581@users.noreply.github.com>
2022-02-06 12:43:58 +0530

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -9,12 +9,12 @@ from bot.core.db.add import add_user_to_database
 @Client.on_message(filters.command(["start", "ping"]) & filters.private & ~filters.edited)
 async def ping_handler(c: Client, m: "types.Message"):
     if not m.from_user:
-        return await m.reply_text("I don't know about you sar :(")
+        return await m.reply_text("Pakkaki pora pumka")
     await add_user_to_database(c, m)
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text="Hi, I am Rename Bot!\n\n"
-             "I can rename media without downloading it!\n"
+        text="Hi vro!\n\n"
+             "Etlunnav ra batta\n"
              "Speed depends on your media DC.\n\n"
              "Just send me media and reply to it with /rename command.",
         reply_markup=types.InlineKeyboardMarkup([[
